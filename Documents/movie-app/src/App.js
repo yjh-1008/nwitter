b816e9856ca103052025a205b355e69957c01c87
yjh-1008 <71313511+yjh-1008@users.noreply.github.com>
2021-08-19 17:27:40 +0900

    finish state 3

--- Documents/movie-app/src/App.js
+++ Documents/movie-app/src/App.js
@@ -1,100 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
-
-
-
-const foodILike = [
-  {
-    id:1,
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-    rating:5
-  },
-  {
-    id:2,
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-    rating:4.9
-  },
-  {
-    id:3,
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-    rating:4.8
-  },
-  {
-    id:4,
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-    rating:5.5
-  },
-  {
-    id:5,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-    rating:4.7
-  }
-];
-
-function Food({name, picture, rating}){
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name}></img>
-    </div>
-  );
-}
-
-
-Food.propTypes = {
-  name:PropTypes.string.isRequired,
-  picture:PropTypes.string.isRequired,
-  rating:PropTypes.number
-};
-
-
 class App extends React.Component{
-  constructor(props){
-    super(props);
-    console.log("hello");
-  }
-
-  state = {
-    count:0
+  state ={
+    isLoading:true
   };
-  add=()=>{
-    this.setState(current=>({count:current.count+1}));
-  };
-  minus=()=>{
-    this.setState(current=>({count:current.count-1}));
-  };
-
   componentDidMount(){
-    console.log("component rendered");
-  }
-
-  componentDidUpdate(){
-    console.log("I just Updated");
-  }
-
-  componentWillUnmount(){
-    console.log("Goodbye, cruel work");
+    setTimeout(()=>{
+      this.setState({isLoading:false});
+    
+  },6000);
   }
   render(){
-    console.log("I'm rendering");
-    return(
-      <div>
-      <h1>The number {this.state.count}</h1>
-      <button onClick={this.add}>Add</button>
-      <button onClick={this.minus}>minus</button>
-      </div>
-    );
+    const {isLoading}=this.state;
+    console.log({isLoading});
+    return<div>{isLoading?"Loading":"We are ready"}</div>;
   }
 
 }
