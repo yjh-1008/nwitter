cfab67036acab10cd3db51e56e40a77e4ca80f03
yjh-1008 <71313511+yjh-1008@users.noreply.github.com>
2021-08-21 02:47:14 +0900

    upload css

--- Documents/movie-app/src/App.js
+++ Documents/movie-app/src/App.js
@@ -1,19 +1,50 @@
 import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
 import PropTypes from "prop-types";
+import "./App.css";
 class App extends React.Component{
   state ={
-    isLoading:true
+    isLoading:true,
+    movies:[]
+  };
+  getMovies = async() =>{
+    const{
+      data:{
+        data:{movies}
+      }
+    }=await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
+    this.setState({movies, isLoading:false})
   };
   componentDidMount(){
-    setTimeout(()=>{
-      this.setState({isLoading:false});
     
-  },6000);
+    this.getMovies();
   }
   render(){
-    const {isLoading}=this.state;
-    console.log({isLoading});
-    return<div>{isLoading?"Loading":"We are ready"}</div>;
+    const {isLoading,movies}=this.state;
+    return(
+      <section className="container">
+          {isLoading?(
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+          ):(
+            <div className="movies">
+            {movies.map(movie=>(
+              <Movie 
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+            />   
+      ))}
+    </div>
+    )}
+    </section>
+    )
   }
 
 }
